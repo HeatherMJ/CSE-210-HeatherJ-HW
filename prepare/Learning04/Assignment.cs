9c9cd280a0414607cf513d3b2bab74b75969c031
HeatherMJ <dhsjewett@gmail.com>
2024-04-04 00:04:24 -0400

    Finished Assignment

---
+++ prepare/Learning04/Assignment.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Learning04
+{
+    public class Assignment
+    {
+        private string _studentName;
+        private string _topic;
+
+        public Assignment(string studentName, string topic)
+        {
+            _studentName = studentName;
+            _topic = topic;
+        }
+        public string GetStudentName()
+        {
+            return _studentName;
+        }
+        public string GetTopic()
+        {
+            return _topic;
+        }
+        public string GetSummary()
+        {
+            return $"{_studentName} and {_topic}";
+
+        }
+    }
+}
